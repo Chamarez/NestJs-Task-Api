f50a54a0100cf0b82f79ac3cda7f03d4e4970e6f
chamarez <michel.chamarez@gmail.com>
2021-11-17 21:25:15 +0100

    First commit structure created

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TaskModule } from './tasks/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'my_db',
+      entities: ['dist/**/*.entity{.ts, .js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+
+    TaskModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
